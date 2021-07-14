6009560635e9acb1f71df71cd6aecf4b4e641b0c
shogo-ppt <68176331+shogo-ppt@users.noreply.github.com>
2021-07-14 22:26:25 +0900

    [Apd]6文字以上の暗号・復号にも対応

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sympy
+import numpy
 import random
 
 
@@ -11,10 +12,10 @@ def main():
             if mode == '0':
                 print(gen_key())
             elif mode == '1':
-                p_text = input('Input plain text (Max:6):')
+                p_text = input('Input plain text: ')
                 print('Encrypted text:', encrypt(p_text, input_key(), mod))
             elif mode == '2':
-                e_text = input('Input encrypted text (Max:6):')
+                e_text = input('Input encrypted text:')
                 print('Decrypted text:', decrypt(e_text, input_key(), mod))
             elif mode == '3':
                 break
@@ -40,19 +41,34 @@ def gen_key():
 
 def text_to_mat(text):
     int_list = [(ord(char) - 32) for char in text]
-    e, f, g, h, i, j = int_list
-    return sympy.Matrix([[e, f, g], [h, i, j]])
+    len_list = len(int_list)
+    if len_list % 2 == 1:
+        int_list.append(31)
+        len_list += 1
+    return sympy.Matrix(2, len_list // 2, int_list)
 
 
 def encrypt(p_text, key, mod):
-    e, f, g, h, i, j = (key * text_to_mat(p_text)) % mod
-    enc_text = ''.join(chr(i + 32) for i in [e, f, g, h, i, j])
+    e_mat = (key * text_to_mat(p_text)) % mod
+    e_mat_len = len(e_mat)
+    e_list = []
+    for i in range(2):
+        for j in range(e_mat_len // 2):
+            e_list.append(e_mat[i, j])
+    enc_text = ''.join(chr(i + 32) for i in e_list)
     return enc_text
 
 
 def decrypt(e_text, key, mod):
-    e, f, g, h, i, j = (key.inv_mod(mod) * text_to_mat(e_text)) % mod
-    dec_text = ''.join(chr(i + 32) for i in [e, f, g, h, i, j])
+    d_mat = (key.inv_mod(mod) * text_to_mat(e_text)) % mod
+    d_mat_len = len(d_mat)
+    d_list = []
+    for i in range(2):
+        for j in range(d_mat_len // 2):
+            d_list.append(d_mat[i, j])
+    if d_list[d_mat_len - 1] == 31:
+        d_list.pop(d_mat_len - 1)
+    dec_text = ''.join(chr(i + 32) for i in d_list)
     return dec_text
 
 
